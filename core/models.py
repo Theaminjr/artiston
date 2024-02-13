a6647bfe625ce97f8941a6f0359f21ba636a1a8a
amin <aminnajarisisi@gmail.com>
2024-02-13 11:08:19 +0330

    create user,profile,address models

---
+++ core/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, phone_number, password, **extra_fields):
+
+        if not phone_number:
+            raise ValueError(_("The phone_number must be set"))
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(phone_number, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(unique = True,max_length = 20)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.phone_number
+
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+
+
+class Address(models.Model):
+    detail = models.TextField()
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
